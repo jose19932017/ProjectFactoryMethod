152f2d24567ca208793991d634f78f84d7a69d02
Jose Luis Martinez Cisneros <jose.martinez@lennken.com>
2023-06-02 18:57:18 -0600

    agregamos las primeras clases para el Factory Method

---
+++ Tools/Earn/LocalEarn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Earn
+{
+    public class LocalEarn : IEarn
+    {
+        private decimal _porcentaje;
+
+        public LocalEarn(decimal porcentaje)
+        {
+            _porcentaje = porcentaje;
+        }
+
+        public decimal Earn(decimal amount)
+        {
+            return amount * _porcentaje;
+        }
+    }
+}
---
+++ Tools/Earn/LocalEarnFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Earn
+{
+    public class LocalEarnFactory : EarnFactory
+    {
+        private decimal _porcentaje;
+
+        public LocalEarnFactory(decimal porcentaje)
+        {
+            _porcentaje = porcentaje;
+        }
+
+        public override IEarn GetEarn()
+        {
+            return new LocalEarn(_porcentaje);
+        }
+    }
+}
