33ad5c6fadb1bc2c7ee4366aaeee1dc21592ea1d
Jose Luis Martinez Cisneros <jose.martinez@lennken.com>
2023-06-02 21:10:35 -0600

    se agrega el cliente

---
+++ Tools/Earn/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Earn
+{
+    public class Cliente
+    {
+        public void main()
+        {
+            decimal valUno = 0.9m;
+            LocalEarnFactory local = new LocalEarnFactory(0.3m);
+            var pruebas = local.GetEarn();
+            var total = pruebas.Earn(valUno);
+            Console.WriteLine(total);
+        }
+    }
+}
